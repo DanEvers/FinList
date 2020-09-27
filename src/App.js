c5b622bc55fc80b4e2da4236d2cfc418b19663a2
Daniel Evers <contact@danielevers.online>
2020-09-27 15:54:28 +0200

    First commit of FinList

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="FinList">
+      <div classname="FinList-head">
+        <h1>Test</h1>
+      </div>
+      <div className="FinList-main">
+        <div className="title">
+          <h1>FinList</h1>
+        </div>
+        <div className="slogan">
+          <h5>Der Atlas für gewerbliche Immobilienfinanzierung</h5>
+        </div>
+
+        {/* <img src={logo} className="FinList-logo" alt="logo" /> */}
+        <div className="slogan2">
+          <h4>Sichern Sie sich den kostenlosen Test - bis zum 31.10!</h4>
+        </div>
+        <div>
+        <button id="button">
+          Test sichern
+        </button>
+        </div>
+      </div>
     </div>
   );
 }
